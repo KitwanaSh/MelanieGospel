911e9bd90dd7de270791f856b75423e186137455
Kitwana Shebabetsi Ezechiel <rodriguekitweze@gmail.com>
2023-05-08 20:13:01 +0200

    Refractor: Separate songs

---
+++ All_song.js
@@ -0,0 +1,83 @@
+//song list
+let All_song = [
+    {
+      name: "Niwezeshe",
+      path: "music/1.mp3",
+      img: "images/1.jpg",
+      singer: "Mélanie Kitwana feat. Prince Mulidwa"
+    },
+    {
+      name: "Niache Ni Wite",
+      path: "music/2.mp3",
+      img: "images/2.jpg",
+      singer: "Mélanie Kitwana"
+    },
+    {
+      name: "Vuta Subira",
+      path: "music/3.mp3",
+      img: "images/3.jpg",
+      singer: "Mélanie Kitwana"
+    },
+    {
+      name: "Tawala",
+      path: "music/4.mp3",
+      img: "images/4.jpg",
+      singer: "Mélanie Kitwana"
+    },
+    {
+      name: "Uwe Na Amani",
+      path: "music/5.mp3",
+      img: "images/5.jpg",
+      singer: "Mélanie Kitwana"
+    },
+    {
+     name: "Safari",
+     path: "music/6.mp3",
+     img: "images/1.jpg",
+     singer: "Mélanie Kitwana"
+    },
+     {
+     name: "Ingiliya kati",
+     path: "music/7.mp3",
+     img: "images/1.jpg",
+     singer: "Mélanie Kitwana"
+    },
+     {
+     name: "Atafany njia",
+     path: "music/8.mp3",
+     img: "images/1.jpg",
+     singer: "Mélanie Kitwana"
+    },
+     {
+     name: "Ebeneza",
+     path: "music/9.mp3",
+     img: "images/1.jpg",
+     singer: "Mélanie Kitwana"
+    },
+     {
+     name: "Jipe raha",
+     path: "music/11.mp3",
+     img: "images/1.jpg",
+     singer: "Mélanie Kitwana"
+    },
+     {
+     name: "Kuna Ushindi",
+     path: "music/12.mp3",
+     img: "images/1.jpg",
+     singer: "Mélanie Kitwana"
+    },
+     {
+     name: "Kwa Injili tuna fahulu",
+     path: "music/13.mp3",
+     img: "images/1.jpg",
+     singer: "Mélanie Kitwana"
+    },
+     {
+     name: "Karibu nawe",
+     path: "music/14.mp3",
+     img: "images/1.jpg",
+     singer: "Mélanie Kitwana"
+    }
+ ];
+
+ export default All_song
